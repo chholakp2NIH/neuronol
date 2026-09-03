c767f2b5ec458941fd304b471113d4a37456e315
Parth Chholak <parthchholak@gmail.com>
2026-09-03 15:36:20 -0400

    remove eegacamp-specific content from utilities

--- neuronol/utilities.py
+++ neuronol/utilities.py
@@ -2,32 +2,10 @@ import re
 from pathlib import Path
 
 
-def create_bids_file_paths_for_eegacamp_recording(recording_data_dir: Path):
-    """
-    Creates file paths for raw iMotions' CSV and other related
-    files found under the recording data path.
-    """
-    subj_id = re.findall(r"^.+/sub-(..)/.+$", str(recording_data_dir))[0]
-    fpaths = {
-        "fpath_import": recording_data_dir / f"sub-{subj_id}_task-all_eeg.csv",
-        "fpath_mne_raw": recording_data_dir / f"sub-{subj_id}_task-all_eeg.fif",
-        "fpath_mne_report": recording_data_dir
-        / f"sub-{subj_id}_task-all_importreport.html",
-        "fpath_headcircum": (
-            recording_data_dir / f"sub-{subj_id}_desc-manual_headcircumference.json"
-        ),
-        "fpath_bs_dig": (
-            recording_data_dir / f"sub-{subj_id}_task-all_acq-polhemus_headshape.mat"
-        ),
-        "event_files": recording_data_dir.glob("*_eventsstimtimes.xlsx"),
-    }
-    return fpaths
-
-
-def get_subj_and_session_from_eegacamp_recording_data_dir(recording_data_dir: Path):
+def get_sub_and_ses_from_bids_rec_dir(recording_data_dir: Path):
     """
     Finds subject and session ids from recording data directory following BIDS format.
     """
-    sub_id = re.findall(r"^.+/sub-(..)/.+$", str(recording_data_dir))[0]
-    ses_id = re.findall(r"^.+/ses-(.+)/.+$", str(recording_data_dir))[0]
+    sub_id = re.findall(r"^.+/sub-(.+?)/.+$", str(recording_data_dir))[0]
+    ses_id = re.findall(r"^.+/ses-(.+?)/.+$", str(recording_data_dir))[0]
     return sub_id, ses_id
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,38 +1,12 @@
 from pathlib import Path
 
-from neuronol.utilities import (
-    create_bids_file_paths_for_eegacamp_recording,
-    get_subj_and_session_from_eegacamp_recording_data_dir,
-)
-
-
-def test_create_bids_file_paths_for_eegacamp_recording():
-    recording_data_dir = (
-        Path.home() / "data/bids/imotions-sample/sub-xx/ses-studyvisit2/eeg"
-    )
-    fpaths = create_bids_file_paths_for_eegacamp_recording(recording_data_dir)
-    assert fpaths["fpath_import"] == (recording_data_dir / "sub-xx_task-all_eeg.csv")
-    assert fpaths["fpath_mne_raw"] == (recording_data_dir / "sub-xx_task-all_eeg.fif")
-    assert fpaths["fpath_mne_report"] == (
-        recording_data_dir / "sub-xx_task-all_importreport.html"
-    )
-    assert fpaths["fpath_headcircum"] == (
-        recording_data_dir / "sub-xx_desc-manual_headcircumference.json"
-    )
-    assert fpaths["fpath_bs_dig"] == (
-        recording_data_dir / "sub-xx_task-all_acq-polhemus_headshape.mat"
-    )
-    assert list(fpaths["event_files"]) == [
-        recording_data_dir / "sub-xx_task-restingstate_eventsstimtimes.xlsx"
-    ]
+from neuronol.utilities import get_sub_and_ses_from_bids_rec_dir
 
 
 def test_get_subj_and_session_from_eegacamp_recording_data_dir():
     recording_data_dir = (
         Path.home() / "data/bids/imotions-sample/sub-xx/ses-studyvisit2/eeg"
     )
-    sub_id, ses_id = get_subj_and_session_from_eegacamp_recording_data_dir(
-        recording_data_dir
-    )
+    sub_id, ses_id = get_sub_and_ses_from_bids_rec_dir(recording_data_dir)
     assert sub_id == "xx"
     assert ses_id == "studyvisit2"
